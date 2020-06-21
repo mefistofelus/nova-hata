4ca660dc23bd7c4969fb3a2c36989d11036b2cb6
mefistofelus <manilo@ukr.net>
2020-06-21 17:23:25 +0300

    инициализация проекта, верстка шапки и первой секции

---
+++ gulpfile.js
@@ -0,0 +1,171 @@
+let project_folder = require("path").basename(__dirname);
+let source_folder = "src";
+
+let fs = require('fs');
+
+let path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+  },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/scss/style.scss",
+    js: source_folder + "/js/script.js",
+    img: source_folder + "/img/**/*.{jpeg,jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/scss/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpeg,jpg,png,svg,gif,ico,webp}",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
+  clean: "./" + project_folder + "/"
+}
+
+let {src, dest} = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  fileinclude = require('gulp-file-include'),
+  del = require('del'),
+  scss = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  group_media = require('gulp-group-css-media-queries'),
+  clean_css = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify-es').default,
+  imagemin = require('gulp-imagemin'),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html'),
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  fonter = require('gulp-fonter');
+
+  function browserSync(params) {
+    browsersync.init({
+      server:{
+        baseDir: "./" + project_folder + "/"
+      },
+      port: 3000,
+      notify: false,
+      //online: online
+    })
+  }
+
+  function html() {
+    return src(path.src.html)
+      //сборка html-файлов
+      .pipe(fileinclude())
+      //авто-подключение изображений формата webp в html-разметке
+      .pipe(webphtml())
+      //копирование html-файлов в папку продакшена
+      .pipe(dest(path.build.html))
+      //перезагрузка браузера
+      .pipe(browsersync.stream())
+  }
+
+  function css() {
+    return src(path.src.css)
+      //сборка scss-файлов
+      .pipe(scss({
+          outputStyle: "expanded"
+        })
+      )
+      .pipe(group_media())
+      .pipe(autoprefixer({
+        overrideBrowserslist: ["last 5 versions"],
+        cascade: true
+      }))
+      //копирование scss-файлов в папку продакшена
+      .pipe(dest(path.build.css))
+      .pipe(clean_css())
+      .pipe(rename({
+        extname: ".min.css"
+      }))
+      //копирование сжатых scss-файлов в папку продакшена
+      .pipe(dest(path.build.css))
+      //перезагрузка браузера
+      .pipe(browsersync.stream())
+  }
+
+  function js() {
+    return src(path.src.js)
+      //сборка js-файлов
+      .pipe(fileinclude())
+      //копирование js-файлов в папку продакшена
+      .pipe(dest(path.build.js))
+      .pipe(uglify())
+      .pipe(rename({
+        extname: ".min.js"
+      }))
+      //копирование сжатых js-файлов в папку продакшена
+      .pipe(dest(path.build.js))
+      //перезагрузка браузера
+      .pipe(browsersync.stream())
+  }
+
+  function images() {
+    return src(path.src.img)
+    .pipe(webp({
+      quality: 70
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    // .pipe(imagemin({
+    //   progressive: true,
+    //   svgoPlugins: [{removeViewBox: false}],
+    //   interlaced: true,
+    //   optimizationLevel: 3 // from 0 to 7
+    // }))
+    //копирование html-файлов в папку продакшена
+    .pipe(dest(path.build.img))
+    //перезагрузка браузера
+    .pipe(browsersync.stream())
+  }
+
+  function fonts(params) {
+    src(path.src.fonts)
+      .pipe(ttf2woff())
+      .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+      .pipe(ttf2woff2())
+      .pipe(dest(path.build.fonts))
+  }
+
+  gulp.task('otf2ttf', function(){
+    return src([source_folder + '/fonts/*.otf'])
+      .pipe(fonter({
+        formats: ['ttf']
+      }))
+      .pipe(dest(source_folder + '/fonts/'));
+  })
+
+  //наблюдение за файлами
+  function watchFiles() {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);
+  }
+
+  //удаление папки продакшена для корректной пересборки проекта
+  function clean() {
+    return del(path.clean);
+  }
+
+  let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
+  let watch = gulp.parallel(build, watchFiles, browserSync);
+
+  exports.fonts = fonts;
+  exports.images = images;
+  exports.js = js;
+  exports.css = css;
+  exports.html = html;
+  exports.build = build;
+  exports.watch = watch;
+  exports.default = watch;
