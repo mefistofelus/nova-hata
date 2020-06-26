16035ec53547abe9d52899c7bea3405628a025e8
mefistofelus <manilo@ukr.net>
2020-06-26 06:54:34 +0300

    верстка блока секции new

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,42 @@
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel();
-});
+@@include('../../node_modules/swiper/js/swiper.min.js');
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+})
